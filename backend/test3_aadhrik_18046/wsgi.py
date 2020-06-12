7ae340ca145423e8f5d3ec55fed86229318a1d70
Crowdbotics <team@crowdbotics.com>
2020-06-12 16:58:17 +0000

    Update models

--- backend/test3_aadhrik_18046/wsgi.py
+++ backend/test3_aadhrik_18046/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test3_aadhrik_18046.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test3_aadhrik_18046.settings")
 
 application = get_wsgi_application()
